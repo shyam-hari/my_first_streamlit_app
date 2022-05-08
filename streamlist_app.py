7937deeaa449edba868dc4101816534b788f24af
shyam-hari <105120930+shyam-hari@users.noreply.github.com>
2022-05-08 10:58:08 +0000

    Update streamlist_app.py
    Use Pandas

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,9 +1,14 @@
 import streamlit
+import pandas
 streamlit.title('My Mom\'s Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-boiled Free-range Egg')
-streamlit.text(🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
                
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')               
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
