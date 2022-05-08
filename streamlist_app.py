503b6ff56302172479151115f53ad3e16df998ef
shyam-hari <105120930+shyam-hari@users.noreply.github.com>
2022-05-08 10:16:18 +0000

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My Not So Heathly Diner')
+streamlit.title('My Mom\'s Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
